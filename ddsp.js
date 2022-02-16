399d76af49571ef83aa9df7bef84f3311290c247
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-02-16 13:03:05 +0800

    Update ddsp.js

--- ddsp.js
+++ ddsp.js
@@ -13,7 +13,7 @@
 *******************************
 [rewrite_local]
 # > 多多视频vip
-^http:\/\/^https:\/\/api.rr.tv.+ url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/ddsp.js
+^http:\/\/^https:\/\/api.rr.tv\/(ad/getAll|user/profile|user/privilege/list) url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/ddsp.js
 [mitm] 
 hostname = api.rr.tv
 *
@@ -21,16 +21,27 @@ hostname = api.rr.tv
 */
 
 var body = $response.body;
-var url = $request.url;
+var urlq = $request.url;
+var objc = JSON.parse(body);
 
-if (url.indexOf('/ad/getAll') != -1) {
-    var obj = JSON.parse(body);
-    obj.data.adList = [];
-    body = JSON.stringify(obj);
-} else if (url.indexOf('/user/privilege/list') != -1) {
-    var obj = JSON.parse(body);
-    obj.data = [{"action":"play","effectObject":"video","id":1,"function":"originalPainting","func":"originalPainting","endTime":1667341767582,"description":"解锁原画","icon":"jiesuoyuanhua"},{"action":"play","effectObject":"video","id":4,"function":"noLimit","func":"noLimit","endTime":1567341767582,"description":"看剧无限制","icon":"kanjuwuxianzhi"},{"action":"play","effectObject":"growth","id":37,"function":"0.4","func":"0.4","endTime":1667341767582,"description":"看剧经验+40%","icon":"jingyanzhijiacheng"},{"action":"send","effectObject":"danmu","id":43,"function":"superBarrageBlue","func":"superBarrageBlue","endTime":1667341767582,"description":"超级弹幕","icon":"chaojidanmu"},{"action":"play","effectObject":"video","id":23,"function":"noAd","func":"noAd","endTime":1667341767582,"description":"看剧无广告","icon":"kanjuwuguanggao"}];
-    
+const 1 = '/ad/getAll'
+
+const 2 ='/user/profile'
+
+const 3 ='/user/privilege/list'
+if (urlq.indexOf(2) != -1) {
+objc.medalList = [{"name":"大魔王","endTime":"2099-02-22 02:44:53","imgUrl":"http://img.rr.tv/cover/20200424/o_1587720799676.png","id":2}];
+
+objc.vipMedal = {"name":"大魔王","endTime":"2099-02-22 02:44:53","imgUrl":"http://img.rr.tv/cover/20200424/o_1587720799676.png","id":2,"isExpired":false};
+
+    body = JSON.stringify(objc);
 }
-body = JSON.stringify(obj);
+if (urlq.indexOf(3) != -1) {
+objc.data = [{"id":1,"effectObject":"video","action":"play","function":"originalPainting","func":"originalPainting","description":"解锁原画","icon":"jiesuoyuanhua","endTime":9999940148000},{"id":4,"effectObject":"video","action":"play","function":"noLimit","func":"noLimit","description":"看剧无限制","icon":"kanjuwuxianzhi","endTime":9999940148000},{"id":23,"effectObject":"video","action":"play","function":"noAd","func":"noAd","description":"看剧无广告","icon":"kanjuwuguanggao","endTime":9999940148000},{"id":43,"effectObject":"danmu","action":"send","function":"superBarrageBlue","func":"superBarrageBlue","description":"超级弹幕","icon":"chaojidanmu","endTime":9999940148000},{"id":46,"effectObject":"video","action":"play","function":"vipVideo","func":"vipVideo","description":"勋章专享剧集","icon":"zhuanxiangjuji","endTime":9999940148000},{"id":45,"effectObject":"mall","action":"sale","function":"mallDiscount","func":"mallDiscount","description":"龙醇商城九折","icon":"longchunshangcheng","endTime":9999940148000},{"id":37,"effectObject":"growth","action":"play","function":"0.4","func":"0.4","description":"看剧经验+40%","icon":"jingyanzhijiacheng","endTime":9999940148000},{"id":25,"effectObject":"nickName","action":"show","function":"nameHighLight","func":"nameHighLight","description":"高亮昵称","icon":"gaoliangnicheng","endTime":9999940148000},{"id":24,"effectObject":"comment","action":"write","function":"highLight","func":"highLight","description":"高亮评论回复","icon":"https://img.rr.tv/static/images/20170926/HighLigthComment@2x.png","endTime":9999940148000},{"id":20,"effectObject":"article","action":"write","function":"highLight","func":"highLight","description":"高亮发帖","icon":"gaoliangfatie","endTime":9999940148000}];
+body = JSON.stringify(objc);
+}
+if (urlq.indexOf(1) != -1) {
+objc = 1;
+body = JSON.stringify(objc);
+}
 $done({body});
