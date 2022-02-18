e3432a3aeedcc9fdb0aec9c0cc6036a49e8caf99
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-02-18 18:09:36 +0800

    Update jysp.js

--- jysp.js
+++ jysp.js
@@ -12,7 +12,7 @@
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local] 
-^https:\/\/api\.8897815\.com\/long_video\/user\/(daily/short_video_watch|info) url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
+^https:\/\/api\.8897815\.com\/long_video\/(advertising|user/info) url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
 [mitm]
 hostname = api.8897815.com
 *
@@ -21,14 +21,14 @@ hostname = api.8897815.com
 var body = $response.body;
 var urlq = $request.url;
 var obj = JSON.parse(body);
-const 1 ='/long_video/user/daily/short_video_watch';
+const 1 ='/long_video/advertising';
 const 2 ='/long_video/user/info';
  if (urlq.indexOf(1) != -1) {
-  obj= {"code":0,"msg":"成功","data":{"freeWatchCount":999999}};
+  delete obj["data"];
   body = JSON.stringify(obj);
 }
  if (urlq.indexOf(2) != -1) {
-  obj= {"code":0,"msg":"成功","data":{"userId":2872318,"nickName":"凉意","machineCode":"oi7totr4zecp96eremp7ts","agentId":0,"avatar":"3","goldCoins":99999,"phoneNumber":"","vipEndTime":"2099-01-01","bindCode":"0","userType":2,"rechargeStatus":1}};
+  obj.["vipEndTime"] = "2099-01-01";
   body = JSON.stringify(obj);
 }
 $done({ body });
