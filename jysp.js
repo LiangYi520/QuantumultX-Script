458f3eca5ac1babaae5e4efa982f10474fb39d1c
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-02-18 17:39:58 +0800

    Update jysp.js

--- jysp.js
+++ jysp.js
@@ -24,11 +24,11 @@ var obj = JSON.parse(body);
 const 1 ='/long_video/user/daily/short_video_watch';
 const 2 ='/long_video/user/info';
  if (urlq.indexOf(1) != -1) {
-  obj.data.freeWatchCount = 99999;
+  obj= {"code":0,"msg":"成功","data":{"freeWatchCount":999999}};
   body = JSON.stringify(obj);
 }
  if (urlq.indexOf(2) != -1) {
-  obj.data.vipEndTime = "2099-01-01";
+  obj= {"code":0,"msg":"成功","data":{"userId":2872318,"nickName":"凉意","machineCode":"oi7totr4zecp96eremp7ts","agentId":0,"avatar":"3","goldCoins":99999,"phoneNumber":"","vipEndTime":"2099-01-01","bindCode":"0","userType":2,"rechargeStatus":1}};
   body = JSON.stringify(obj);
 }
 $done({ body });
