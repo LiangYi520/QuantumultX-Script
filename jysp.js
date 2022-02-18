d9c48562befb9f914f79b8a6676cc2d7cf41e6c7
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-02-18 12:39:38 +0800

    Update jysp.js

--- jysp.js
+++ jysp.js
@@ -19,10 +19,10 @@ hostname = api.8897815.com
 *
 */
 var body = $response.body;
- var urlq = $request.url;
- var obj = JSON.parse(body);
- const 1 = '/long_video/user/daily/short_video_watch';
- const 2 = '/long_video/user/info';
+var urlq = $request.url;
+var obj = JSON.parse(body);
+ const 1 ='/long_video/user/daily/short_video_watch';
+ const 2 ='/long_video/user/info';
  if (urlq.indexOf(1) != -1) {
   obj.data.freeWatchCount = 99999;
   body = JSON.stringify(obj);
