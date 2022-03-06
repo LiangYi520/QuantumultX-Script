c72f3be718fbce490237b9b98d3a3d2e610bf403
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-06 15:44:43 +0800

    Delete bilibili.js

--- bilibili.js
+++
@@ -1,4 +0,0 @@
-[rewrite_local]
-^https:\/\/.+\.bilibili\.com/.+ url request-header (\r\n)Authorization.+(\r\n) request-header $1Authorization:identify_v1 d07aa0b52e59f4629ef73bbcd32d9431$2
-[mitm] 
-hostname = *.bilibili.com
