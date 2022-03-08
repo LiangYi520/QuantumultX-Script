5e8f6e62cc16a9317ad95c43c8925bad409f7a63
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-08 14:52:29 +0800

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,4 @@
 [rewrite_local]
 
 # >b
-^https:\/\/app\.bilibili\.com\/bilibili\.*\.gateway\.player\.v2\.PlayURL\/PlayView url script-request-header https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/b.js
+^https:\/\/app\.bilibili\.com\/bilibili\.pgc\.gateway\.player\.v2\.PlayURL\/PlayView url script-request-header https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/b.js
