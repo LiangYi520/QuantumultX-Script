25bfa0c9f720bbe2590c962d6e67ed66dc3364a6
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-08 14:56:36 +0800

    Update b.js

--- b.js
+++ b.js
@@ -2,3 +2,17 @@
 
 # >b
 ^https:\/\/app\.bilibili\.com\/bilibili\.pgc\.gateway\.player\.v2\.PlayURL\/PlayView url script-request-header https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/b.js
+
+[mitm] 
+
+hostname = app.bilibili.com
+*
+*
+*/
+
+
+var ly = $request.headers;
+ly['Authorization'] = 'identify_v1 4aeaadd37a670f85e503f4752d419c31';
+$done({
+    headers : ly
+});
