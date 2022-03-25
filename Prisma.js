0c93535022fa070de8628a52adf5f5de75053f40
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-26 07:39:16 +0800

    Update Prisma.js

--- Prisma.js
+++ Prisma.js
@@ -19,27 +19,5 @@ hostname = api.neuralprisma.com
 *
 *
 */
-var obj = JSON.parse($response.body);
-    obj = {
-  "status": "ok",
-  "is_valid": true,
-  "expiration_date": "2066-03-25T23:38:12Z",
-  "expiration_date_unix": 9648251492,
-  "is_trial": false,
-  "processing_count": 0,
-  "is_introductory_used": true,
-  "product_id": "",
-  "promotional_offer_id": "",
-  "is_grace_period": false,
-  "auto_renew_enabled": true,
-  "is_in_billing_retry_period": false,
-  "subscription_type": "annual",
-  "platform": "ios",
-  "is_bonus": false,
-  "bonus_days_added": 0,
-  "device_user_info": {
-    "auth_type": "",
-    "subscription_valid": true
-  }
-}
-$done({body:JSON.stringify(obj)});
+
+;var encode_version = 'jsjiami.com.v5', xgwxs = '__0xdc3c9',  __0xdc3c9=['GQU5w4U=','IiR7wp7DqjMmwoXDihl/w67CoSDCvk7CjQvDoMKo','w6bDj8KuHQZ5','SMOgwqY=','5YmJ6Zqc54iA5p+e5Y6477ybw5MO5Lyb5ayh5p+l5b2v56mw','Gis5w5XCrQ==','F8Osw47CsmE=','Fh8Fw7jCsg==','M8O5IMKcwqQ=','JsOEDsOidw==','DRzComTCuw==','UkRNwqdj'];(function(_0x5f0ceb,_0x2fcd1a){var _0x13e1e8=function(_0x58315e){while(--_0x58315e){_0x5f0ceb['push'](_0x5f0ceb['shift']());}};_0x13e1e8(++_0x2fcd1a);}(__0xdc3c9,0xbf));var _0x41da=function(_0x90b5dd,_0x26ae2b){_0x90b5dd=_0x90b5dd-0x0;var _0x30992e=__0xdc3c9[_0x90b5dd];if(_0x41da['initialized']===undefined){(function(){var _0x548007=typeof window!=='undefined'?window:typeof process==='object'&&typeof require==='function'&&typeof global==='object'?global:this;var _0xfa522f='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';_0x548007['atob']||(_0x548007['atob']=function(_0x4bcc45){var _0x1193a1=String(_0x4bcc45)['replace'](/=+$/,'');for(var _0x48cb6a=0x0,_0xf27ba7,_0x40e2ad,_0x420a9f=0x0,_0x154d17='';_0x40e2ad=_0x1193a1['charAt'](_0x420a9f++);~_0x40e2ad&&(_0xf27ba7=_0x48cb6a%0x4?_0xf27ba7*0x40+_0x40e2ad:_0x40e2ad,_0x48cb6a++%0x4)?_0x154d17+=String['fromCharCode'](0xff&_0xf27ba7>>(-0x2*_0x48cb6a&0x6)):0x0){_0x40e2ad=_0xfa522f['indexOf'](_0x40e2ad);}return _0x154d17;});}());var _0x6f41f=function(_0x265771,_0x2b6295){var _0x2a452=[],_0x363c5e=0x0,_0x42c273,_0x2b429d='',_0x20f27b='';_0x265771=atob(_0x265771);for(var _0xcec82a=0x0,_0x5127eb=_0x265771['length'];_0xcec82a<_0x5127eb;_0xcec82a++){_0x20f27b+='%'+('00'+_0x265771['charCodeAt'](_0xcec82a)['toString'](0x10))['slice'](-0x2);}_0x265771=decodeURIComponent(_0x20f27b);for(var _0x5ea6e6=0x0;_0x5ea6e6<0x100;_0x5ea6e6++){_0x2a452[_0x5ea6e6]=_0x5ea6e6;}for(_0x5ea6e6=0x0;_0x5ea6e6<0x100;_0x5ea6e6++){_0x363c5e=(_0x363c5e+_0x2a452[_0x5ea6e6]+_0x2b6295['charCodeAt'](_0x5ea6e6%_0x2b6295['length']))%0x100;_0x42c273=_0x2a452[_0x5ea6e6];_0x2a452[_0x5ea6e6]=_0x2a452[_0x363c5e];_0x2a452[_0x363c5e]=_0x42c273;}_0x5ea6e6=0x0;_0x363c5e=0x0;for(var _0x383594=0x0;_0x383594<_0x265771['length'];_0x383594++){_0x5ea6e6=(_0x5ea6e6+0x1)%0x100;_0x363c5e=(_0x363c5e+_0x2a452[_0x5ea6e6])%0x100;_0x42c273=_0x2a452[_0x5ea6e6];_0x2a452[_0x5ea6e6]=_0x2a452[_0x363c5e];_0x2a452[_0x363c5e]=_0x42c273;_0x2b429d+=String['fromCharCode'](_0x265771['charCodeAt'](_0x383594)^_0x2a452[(_0x2a452[_0x5ea6e6]+_0x2a452[_0x363c5e])%0x100]);}return _0x2b429d;};_0x41da['rc4']=_0x6f41f;_0x41da['data']={};_0x41da['initialized']=!![];}var _0x5b7732=_0x41da['data'][_0x90b5dd];if(_0x5b7732===undefined){if(_0x41da['once']===undefined){_0x41da['once']=!![];}_0x30992e=_0x41da['rc4'](_0x30992e,_0x26ae2b);_0x41da['data'][_0x90b5dd]=_0x30992e;}else{_0x30992e=_0x5b7732;}return _0x30992e;};var obj=JSON[_0x41da('0x0','a*7f')]($response[_0x41da('0x1','SXk&')]);obj={'status':'ok','is_valid':!![],'expiration_date':_0x41da('0x2','i(Q)'),'expiration_date_unix':0x23f14a264,'is_trial':![],'processing_count':0x0,'is_introductory_used':!![],'product_id':'','promotional_offer_id':'','is_grace_period':![],'auto_renew_enabled':!![],'is_in_billing_retry_period':![],'subscription_type':_0x41da('0x3','w5kD'),'platform':'ios','is_bonus':![],'bonus_days_added':0x0,'device_user_info':{'auth_type':'','subscription_valid':!![]}};$done({'body':JSON['stringify'](obj)});;(function(_0x41b23d,_0x596fe2,_0x33c5d8){var _0x29f9b5={'kcNYP':_0x41da('0x4','jg(s'),'aAdij':function _0xa6fdf(_0x2531f7,_0x1426a8){return _0x2531f7!==_0x1426a8;},'ivmlo':'undefined','muXDu':'jsjiami.com.v5','XSbAp':function _0x18c890(_0x4cf059,_0x185f1c){return _0x4cf059+_0x185f1c;},'wDqOm':'版本号，js会定期弹窗，还请支持我们的工作','anhIE':_0x41da('0x5','89rl')};_0x33c5d8='al';try{_0x33c5d8+=_0x29f9b5['kcNYP'];_0x596fe2=encode_version;if(!(_0x29f9b5[_0x41da('0x6','SXk&')](typeof _0x596fe2,_0x29f9b5[_0x41da('0x7','MDsS')])&&_0x596fe2===_0x29f9b5[_0x41da('0x8','SXk&')])){_0x41b23d[_0x33c5d8](_0x29f9b5[_0x41da('0x9','*kJo')]('删除',_0x29f9b5[_0x41da('0xa','UiG!')]));}}catch(_0x244ae0){_0x41b23d[_0x33c5d8](_0x29f9b5[_0x41da('0xb','PvL0')]);}}(window));;encode_version = 'jsjiami.com.v5';
