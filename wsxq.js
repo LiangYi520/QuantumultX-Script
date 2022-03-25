56704d7697f3622b4d58198c8931f0fb4490d695
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-26 06:58:27 +0800

    Create wsxq.js

---
+++ wsxq.js
@@ -0,0 +1,25 @@
+/*
+ *
+ *
+脚本功能：解锁vip
+软件版本：10.4.40
+下载地址：http://share.momosyb.com/?id=9627063
+脚本作者：凉意
+更新时间：2022.03.26
+电报频道：https://t.me/liangyiA
+问题反馈：https://t.me/LiangYi66
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+[rewrite_local]
+# > 微商星球解锁vip
+^https:\/\/api\.momosyb\.com\/v6/\user\/index$ url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/wsxq.js
+[mitm] 
+hostname = api.momosyb.com
+*
+*
+*/
+var obj = JSON.parse($response.body);
+    obj.data.isvip = 1;
+
+$done({body:JSON.stringify(obj)});
