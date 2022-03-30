16b7bb98df109efd34a38d790a11df7162b26439
凉意 <99691000+LiangYi520@users.noreply.github.com>
2022-03-30 09:44:42 +0800

    Update jysp.js

--- jysp.js
+++ jysp.js
@@ -1,29 +1,47 @@
 /*
  *
  *
-脚本功能：鲸鱼视频无限观看
-软件版本：极速版
-下载地址：https://6478726.com/q4r3p.html?code=9ddU2bd3fe
+脚本功能：破解鲸鱼视频，去启动广告
+软件版本：
+下载地址：https://6478726.com/yn0ma.html?code=9ddU5357c9
 脚本作者：凉意
-更新时间：2022.02.18
+更新时间：2022.03.30
 电报频道：https://t.me/liangyiA
 问题反馈：https://t.me/LiangYi66
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 [rewrite_local]
-^https:\/\/api\.8897815\.com\/long_video\/advertising url response-body LOGIN_BEFOR response-body 1
-[rewrite_local] 
-^https:\/\/api\.8897815\.com\/long_video\/user\/info url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
-[mitm]
-hostname = api.8897815.com
+# > 鲸鱼视频破解
+^https:\/\/res\.czdxgbh2020\.com\/(long_video\/user\/daily\/short_video_watch|long_video\/user\/info|long_video\/user\/video\/buy|long_video\/advertising)$ url script-response-body https://raw.githubusercontent.com/LiangYi520/QuantumultX-Script/main/jysp.js
+[mitm] 
+hostname = res.czdxgbh2020.com
 *
 *
 */
-var objc = JSON.parse($response.body);
+let ly = $request.url;
+const path1 = "long_video/user/daily/short_video_watch";
+const path2 = "long_video/user/info";
+const path3 = "long_video/user/video/buy";
+const path3 = "long_video/advertising";
+let obj = JSON.parse($response.body);
 
-objc.data["nickName"] = "凉意";
-objc.data["vipEndTime"] = "2099-09-28";
-objc.data["userType"] = 1;
+if (ly.indexOf(path4) != -1) {
+  delete obj["data"];
+  
+}
+if (ly.indexOf(path2) != -1) {
+  obj["data"]["nickName"] = "凉意";
+  obj["data"]["vipEndTime"] = "2099-01-01";
+  obj["data"]["userType"] = 2;
+}
+if (ly.indexOf(path3) != -1) {
+  obj["data"]["isSuccess"] = true;
+}
+if (ly.indexOf(path1) != -1) {
+  obj["data"]["freeWatchCount"] = 999990;
+}
 
-$done({body : JSON.stringify(objc)});
+$done({
+  body: JSON.stringify(obj)
+});
